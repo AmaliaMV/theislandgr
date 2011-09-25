5c24bdd41cf665bf03a6231eb53a66e873c63a42
amalia.0205@gmail.com <amalia.0205@gmail.com@56b5fde7-4276-3573-20ec-a889f6e965ac>
2011-09-25 20:53:30 +0000

    Encapsulamiento de math

---
+++ src/src/matematica/Matematica.h
@@ -0,0 +1,31 @@
+/*
+ * Matematica.h
+ *
+ *  Created on: 21/04/2011
+ *      Author: Amalia
+ */
+
+#ifndef MATEMATICA_H_
+#define MATEMATICA_H_
+
+#include <math.h>
+
+class Matematica {
+
+public:
+
+	static const double PI;
+
+	static double cosHex(double anguloHex);
+	static double sinHex(double anguloHex);
+
+	static double cosRad(double anguloRad);
+	static double sinRad(double anguloRad);
+
+	virtual ~Matematica();
+
+private:
+	Matematica();
+};
+
+#endif /* MATEMATICA_H_ */
