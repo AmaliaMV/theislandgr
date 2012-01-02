6ee9f46c72e0f54893197b6e4c2c1c5426a37ae1
amalia.0205@gmail.com <amalia.0205@gmail.com@56b5fde7-4276-3573-20ec-a889f6e965ac>
2012-01-02 20:31:08 +0000

    agrego el Baupres del barco

---
+++ trunk/src/objetos/barco/mastil/TBaupres.cpp
@@ -0,0 +1,19 @@
+/*
+ * TBaupres.cpp
+ *
+ *  Created on: 02/01/2012
+ *      Author: amalia
+ */
+
+#include "TBaupres.h"
+
+TBaupres::TBaupres() {}
+
+TBaupres::~TBaupres() {}
+
+void TBaupres::ejecutar()
+{
+	glTranslatef(0.0,CteBarco::RADIO_Y,0.0);
+	glScalef(50,50,25);
+	glRotatef(-45, 1.0, 0.0, 0.0);
+}
