80761f06fd101f4742f9c764aad60847148dd7b2
amalia.0205@gmail.com <amalia.0205@gmail.com@56b5fde7-4276-3573-20ec-a889f6e965ac>
2012-01-02 20:27:12 +0000

    manejador de archivos de Ptos de Control

--- src/archivos/adminArchPtosCrtl/ArchivoPtosControl.h
+++ src/archivos/adminArchPtosCrtl/ArchivoPtosControl.h
@@ -11,8 +11,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../matematica/curva/PuntosControl.h"
-#include "EArchivoInexistente.h"
+#include "../../matematica/curva/PuntosControl.h"
+#include "../EArchivoInexistente.h"
 
 struct archEncabezado
 {
