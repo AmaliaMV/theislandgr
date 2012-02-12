f6c3930002983a238a34f077334eee091cdf07d2
amalia.0205@gmail.com <amalia.0205@gmail.com@56b5fde7-4276-3573-20ec-a889f6e965ac>
2012-02-12 03:01:26 +0000

    por las dudas.. tpfinal yfuncionesAux

--- trunk/src/funcionesAux.h
+++ trunk/src/funcionesAux.h
@@ -11,12 +11,22 @@
 #include "mouse/Mouse.h"
 #include "objetos/Mundo.h"
 
-#include "camaras/AdminCamaras.h"
+#include "comandos/AdminComandos.h"
+#include "comandos/VerCamaraMundo.h"
+#include "comandos/VerCamaraCastillo.h"
+#include "comandos/VerCamaraCanon.h"
+#include "comandos/VerCamaraBarco.h"
+#include "comandos/Pausar.h"
+#include "comandos/LanzarBomba.h"
+
 #include "camaras/CamaraMundo.h"
 #include "camaras/CamaraBarco.h"
 #include "camaras/CamaraCanon.h"
+#include "camaras/CamaraCastillo.h"
+
 
 #include "paneles/MCmdJuegos.h"
+#include "paneles/Dimensiones.h"
 
 #include <iostream>
 using namespace std;
@@ -24,26 +34,66 @@ using namespace std;
 Mundo *mundo;
 MCmdJuegos *mCmd;
 AdminCamaras *adminCamaras;
-IMouse *mouse = new Mouse();
-unsigned int camActual = 2;
+AdminComandos *adminComandos;
+IMouse *mouse;
+static const string archivoDeConfiguracion = "archivosNivel1";
 
 float eyeX=0, eyeY=0, eyeZ=0, atX=0, atY=0, atZ=0;
-
-// Variables que controlan la ubicación de la cámara en la Escena 3D
-float eye[3] = {5.0, 10.0, 8.0};
-float at[3]  = { 0.0,  0.0, 0.0};
-float up[3]  = { 0.0,  0.0, 1.0};
-
-// Tamaño de la ventana
-GLfloat window_size[2];
-#define W_WIDTH window_size[0]
-#define W_HEIGHT window_size[1]
 
 #define TOP_VIEWB_POSX	((int)((float)W_WIDTH*0.60f))
 #define TOP_VIEWB_W		((int)((float)W_WIDTH*0.40f))
 #define TOP_VIEWB_POSY	((int)((float)W_HEIGHT*0.20f))
 #define TOP_VIEWB_H		((int)((float)W_HEIGHT*0.40f))
 
+void inicializar()
+{
+	try
+	{
+		mouse = new Mouse();
+		mundo = new Mundo( archivoDeConfiguracion );
+
+		adminCamaras = new AdminCamaras();
+
+		adminCamaras->agregarCamara( CAMARA_MUNDO, new CamaraMundo() );
+		adminCamaras->agregarCamara( CAMARA_BARCO, new CamaraBarco(mundo->getTBarco()) );
+		adminCamaras->agregarCamara( CAMARA_CANON, new CamaraCanon(mundo->getTBarco(), mundo->getBarco()->getTCanon()) );
+		adminCamaras->agregarCamara( CAMARA_CASTILLO, new CamaraCastillo() );
+		adminCamaras->setCamaraActual( CAMARA_MUNDO );
+
+		adminComandos = new AdminComandos();
+		adminComandos->agregarComando('1', new VerCamaraMundo(adminCamaras));
+		adminComandos->agregarComando('2', new VerCamaraBarco(adminCamaras));
+		adminComandos->agregarComando('3', new VerCamaraCanon(adminCamaras));
+		adminComandos->agregarComando('4', new VerCamaraCastillo(adminCamaras));
+		adminComandos->agregarComando('p', new Pausar(mundo));
+		adminComandos->agregarComando('j', new LanzarBomba(mundo));
+
+
+		mCmd = new MCmdJuegos("texturas/menu_comandos.bmp");
+	}
+
+	catch ( EArchivoInexistente *e )
+	{
+		cout<< e->what() <<endl;
+		exit(1);
+	}
+	catch ( ECamara*e )
+	{
+		cout<< e->what() <<endl;
+		exit(1);
+	}
+	catch ( EComando*e )
+	{
+		cout<< e->what() <<endl;
+		exit(1);
+	}
+}
+
+void reshape (int w, int h)
+{
+	TamanoPantalla::setAlto(h);
+	TamanoPantalla::setAncho(w);
+}
 
 void alMoverPresionandoBoton ( int xMouse, int yMouse )
 {
@@ -57,19 +107,26 @@ void alPresionarBoton ( int button, int state, int xMouse, int yMouse )
 {
 }
 
+void dibujar3D()
+{
+	glDisable(GL_LIGHTING);
+		glColor3f(1.0, 1.0, 1.0);
+		mundo->dibujar();
+	glEnable(GL_LIGHTING);
+}
+
+void OnIdle (void)
+{
+	mundo->actualizar();
+	glutPostRedisplay();
+}
+
 void actualizarVista()
 {
 	//seteo la camara actual
 	((Mouse*)mouse)->setCamara(adminCamaras->getCamaraActual()); // esto no deberia hacerse una vez por cambio de camara?
 
 	adminCamaras->actualizarVista();
-//	glPushMatrix();
-//		glBegin( GL_LINE );
-//		glVertex3f(camaras[camActual]->getEye()[0], camaras[camActual]->getEye()[1], camaras[camActual]->getEye()[2]);
-//		glVertex3f(camaras[camActual]->getAt()[0], camaras[camActual]->getAt()[1],camaras[camActual]->getAt()[2]);
-//
-//
-//	glPopMatrix();
 }
 
 #endif /* FUNCIONESAUX_H_ */
--- trunk/src/tpFinal.cpp
+++ trunk/src/tpFinal.cpp
@@ -1,98 +1,18 @@
 #include <iostream>
 #include "funcionesAux.h"
 
-static const string archivoDeConfiguracion = "archivosNivel1";
-
 // Variables asociadas a única fuente de luz de la escena
 float light_color[4] = {1.0f, 1.0f, 1.0f, 1.0f};
 float light_position[3] = {10.0f, 10.0f, 8.0f};
 float light_ambient[4] = {0.05f, 0.05f, 0.05f, 1.0f};
 
 // Variables de control
-bool view_grid = true;
-bool view_axis = true;
-bool edit_panelA = false;
 bool edit_panelB = false;
 
 // Handle para el control de las Display Lists
 GLuint dl_handle;
-#define DL_AXIS (dl_handle+0)
-#define DL_GRID (dl_handle+1)
+#define DL_GRID (dl_handle+0)
 
-void dibujarCentral()
-{
-//	cout<<"eye: x: "<< eyeX << " y: "<< eyeY<<" z: "<<eyeZ<<endl;
-//	cout<<"at: x: "<< atX << " y: "<< atY<<" z: "<<atZ<<endl;
-
-	glDisable(GL_LIGHTING);
-//		glBegin(GL_LINES);
-//
-//			glLineWidth(10);
-//			glColor3f(1.0, 0.0,1.0);
-//			glVertex3f(0.0, 0.0, 0.0);
-//			glVertex3f(eyeX/4, eyeY/4, eyeZ/4);
-//			glColor3f(1.0, 1.0,0.0);
-//			glVertex3f(0.0, 0.0, 0.0);
-//			glVertex3f(atX, atY, atZ);
-//
-////			glVertex3f(0.0, 0.0, 7.0);
-////			glVertex3f(9, 10, 7);
-//		glEnd();
-//		glLineWidth(1);
-
-		mundo->dibujar();
-
-	glEnable(GL_LIGHTING);
-}
-
-void OnIdle (void)
-{
-	mundo->actualizar();
-////    dynamicsWorld->stepSimulation(1/300.f,10);
-//	fisica->getMundoFisico()->stepSimulation(1./60.);//ms / 1000000.f);
-
-	glutPostRedisplay();
-}
-
-void DrawAxis()
-{
-	glDisable(GL_LIGHTING);
-	glBegin(GL_LINES);
-		// X
-		glColor3f(1.0, 0.0, 0.0);
-		glVertex3f(0.0, 0.0, 0.0);
-		glColor3f(0.0, 0.0, 0.0);
-		glVertex3f(15.0, 0.0, 0.0);
-		// Y
-		glColor3f(0.0, 1.0, 0.0);
-		glVertex3f(0.0, 0.0, 0.0);
-		glColor3f(0.0, 0.0, 0.0);
-		glVertex3f(0.0, 15.0, 0.0);
-		// Z
-		glColor3f(0.0, 0.0, 1.0);
-		glVertex3f(0.0, 0.0, 0.0);
-		glColor3f(0.0, 0.0, 0.0);
-		glVertex3f(0.0, 0.0, 15.0);
-	glEnd();
-	glEnable(GL_LIGHTING);
-}
-
-void DrawXYGrid()
-{
-	int i;
-	glDisable(GL_LIGHTING);
-	glColor3f(0.15, 0.1, 0.1);
-	glBegin(GL_LINES);
-	for(i=-20; i<21; i++)
-	{
-		glVertex3f(i, -20.0, 0.0);
-		glVertex3f(i,  20.0, 0.0);
-		glVertex3f(-20.0, i, 0.0);
-		glVertex3f( 20.0, i, 0.0);
-	}
-	glEnd();
-	glEnable(GL_LIGHTING);
-}
 void Set3DEnv()
 {
 	glViewport (0, 0, (GLsizei) W_WIDTH, (GLsizei) W_HEIGHT);
@@ -108,10 +28,9 @@ void SetPanelTopEnvB()
     glLoadIdentity ();
 	gluOrtho2D(-0.10, 1.05, -0.10, 1.05);
 }
-
 void init(void)
 {
-	dl_handle = glGenLists(2);
+	dl_handle = glGenLists(1);
 
 	glClearColor (0.02, 0.02, 0.04, 0.0);
     glShadeModel (GL_SMOOTH);
@@ -123,34 +42,10 @@ void init(void)
     glEnable(GL_LIGHTING);
 
 	// Generación de las Display Lists
-	glNewList(DL_AXIS, GL_COMPILE);
-		DrawAxis();
-	glEndList();
 	glNewList(DL_GRID, GL_COMPILE);
-		DrawXYGrid();
+//		DrawXYGrid();
 	glEndList();
-
-	glColor3f(0.0,0.0,0.0);
-
-	try
-	{
-		mundo = new Mundo( archivoDeConfiguracion );
-
-		adminCamaras = new AdminCamaras();
-
-		adminCamaras->agregarCamara( CAMARA_MUNDO, new CamaraMundo() );
-		adminCamaras->agregarCamara( CAMARA_BARCO, new CamaraBarco(mundo->getTBarco()) );
-		adminCamaras->agregarCamara( CAMARA_CANON, new CamaraCanon(mundo->getTBarco(), mundo->getBarco()->getTCanon()) );
-		adminCamaras->setCamaraActual( CAMARA_MUNDO );
-
-		mCmd = new MCmdJuegos("texturas/menu_comandos.bmp");
-	}
-	catch ( EArchivoInexistente *e )
-	{
-		cout<< e->what() <<endl;
-		exit(1);
-	}
-
+	inicializar();
 }
 
 
@@ -165,31 +60,14 @@ void display(void)
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	//seteo el eye y at de la camara actual
-	actualizarVista();
-
-	if (view_axis)
-		 glCallList(DL_AXIS);
-
-	if (view_grid)
-		 glCallList(DL_GRID);
-	//
-	///////////////////////////////////////////////////
-
-		dibujarCentral();
+	actualizarVista(); //seteo el eye y at de la camara actual
+	dibujar3D();
 
 	///////////////////////////////////////////////////
-	// Panel 2D para la vista superior
-	if (edit_panelA)
-	{
-		mCmd->dibujarPanel( W_WIDTH, W_HEIGHT );
-//		SetPanelTopEnvA();
-//		glMatrixMode(GL_MODELVIEW);
-//		glLoadIdentity();
-//		gluLookAt (0, 0, 0.5, 0, 0, 0, 0, 1, 0);
-//
-//		glCallList(DL_AXIS2D_TOP);
-	}
+	// Paneles 2D
+
+	mCmd->dibujar();
+
 
 	if (edit_panelB)
 	{
@@ -205,145 +83,65 @@ void display(void)
 			glVertex3f (0.5, 1.0, 0.0);
 			glLineWidth (1.0);
 		glEnd();
-
-//		glCallList(DL_AXIS2D_TOP);
 	}
-	//
-	///////////////////////////////////////////////////
-
 
 	glutSwapBuffers();
 }
 
-void reshape (int w, int h)
-{
-   	W_WIDTH  = w;
-	W_HEIGHT = h;
-}
-
-
-
-void initPhysics()
-{
-//	btTransform startTransform;
-//	startTransform.setIdentity();
-//
-
-//	// Defino Esfera
-//
-//	btCollisionShape* esferaShape = new  btSphereShape(radioEsfera);
-//	mass = 10;
-//	esferaShape->calculateLocalInertia(mass,fallInertia);
-//
-//	startTransform.setIdentity();
-//	startTransform.setOrigin(btVector3(btScalar(posicionEsferaRB[0]),btScalar(posicionEsferaRB[1]),btScalar(posicionEsferaRB[2])));
-//	btDefaultMotionState* esferaMotionState = new btDefaultMotionState(startTransform);
-//
-//	btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI2(mass,esferaMotionState,esferaShape,fallInertia);
-//    esferaRB = new btRigidBody(fallRigidBodyCI2);
-//	// lo defino  como Kinetic Rigid Body
-//
-//	esferaRB->setCollisionFlags( esferaRB->getCollisionFlags() | btCollisionObject::CF_KINEMATIC_OBJECT);
-//	esferaRB->setActivationState(DISABLE_DEACTIVATION);
-//
-//	dynamicsWorld->addRigidBody(esferaRB);
-//
-//
-//	// Defino Cubo
-//
-//	btCollisionShape* cuboShape = new  btBoxShape(btVector3(1.0,1.0,1.0));
-//	mass = 10;
-//	cuboShape->calculateLocalInertia(mass,fallInertia);
-//
-//	startTransform.setIdentity();
-//	startTransform.setOrigin(btVector3(btScalar(posicionCuboRB[0]),btScalar(posicionCuboRB[1]),btScalar(posicionCuboRB[2])));
-//	startTransform.setRotation(btQuaternion(btScalar(0),btScalar(0),btScalar(45)));
-//	btDefaultMotionState* cuboMotionState = new btDefaultMotionState(startTransform);
-//
-//	btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI3(mass,cuboMotionState,cuboShape,fallInertia);
-//    cuboRB = new btRigidBody(fallRigidBodyCI3);
-//	cuboRB->setFriction(0.00);
-//	dynamicsWorld->addRigidBody(cuboRB);
-
-
- }
-
 void keyboard (unsigned char key, int x, int y)
 {
+
 	cout<<"key: "<<(unsigned int)key<<endl;
+
+	if ( key == 'q' )
+	{
+  	  delete mCmd;
+  	  delete adminCamaras;
+  	  delete mouse;
+  	  delete adminComandos;
+  	  delete mundo;
+      exit(0);
+	}
+
+	adminComandos->ejecutarComando( key );
 
    switch (key) {
 
    case 'm':
-	   edit_panelA = !edit_panelA;
+	   mCmd->modificarMostrar();
+	   if ( mundo->estaPausado() == false)
+		   mundo->pausar();
 	   break;
 
-      case 'q':
-    	  delete mundo;
-    	  delete mCmd;
-    	  delete adminCamaras;
-         exit(0);
-         break;
-	  case 'g':
-		  view_grid = !view_grid;
-		  glutPostRedisplay();
-		  break;
-
-	  case 'a':
-		  view_axis = !view_axis;
-		  glutPostRedisplay();
-		  break;
-	  case '1':
-		  adminCamaras->setCamaraActual( CAMARA_MUNDO );
-		  break;
-	  case '2':
-		  adminCamaras->setCamaraActual( CAMARA_BARCO );
-		  break;
-	  case '3':
-		  adminCamaras->setCamaraActual( CAMARA_CANON );
-//		  edit_panelB = !edit_panelB;
-//		  glutPostRedisplay();
-		  break;
 	  case 'r':
 		  mundo->reiniciarFisica();
 		  glutPostRedisplay();
 		  break;
 
-	  case 'k':
-
-		  glutPostRedisplay();
-		  break;
-	  case 'l':
-
-		  glutPostRedisplay();
-		  break;
 	  case 'z':
 		  adminCamaras->getCamaraActual()->alejarCamara();
 		  break;
 	  case 'x':
 		  adminCamaras->getCamaraActual()->acercarCamara();
 		  break;
-	  case 'p':
+	  case 'u':
 		  adminCamaras->getCamaraActual()->subirCamara();
 		  break;
-	  case 'o':
-		  adminCamaras->getCamaraActual()->alejarCamara();
+	  case 'i':
+		  adminCamaras->getCamaraActual()->bajarCamara();
 		  break;
-
 	  case 'c':
-//		  mundo->getBarco()->getCanon()->decAngV();
+		  mundo->getBarco()->getCanon()->decAngV();
 		  break;
 	  case 'v':
-//		  mundo->getBarco()->getCanon()->incAngV();
+		  mundo->getBarco()->getCanon()->incAngV();
 		  break;
 	  case 'b':
-//		  mundo->getBarco()->getCanon()->izquierda();
+		  mundo->getBarco()->getCanon()->izquierda();
 		  break;
 	  case 'n':
-//		  mundo->getBarco()->getCanon()->derecha();
+		  mundo->getBarco()->getCanon()->derecha();
 		  break;
-
-
      default:
          break;
    }
